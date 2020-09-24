5b70d4204b0e895631f8c99252633ff936d1bd46
MercedesDavila <mmdavila@outlook.com>
2020-09-23 19:20:21 -0500

    Configurando Redux en el proyecto (Sección 1 y 2)

---
+++ src/redux/userDuck.js
@@ -0,0 +1,19 @@
+// constants
+
+let initialData ={
+    loggedIn: false
+}
+
+let LOGIN ="LOGIN";
+
+//reducer
+
+export default function reducer(state = initialData, action){
+    switch(action.type){
+        case LOGIN:
+        default:
+            return state
+    }
+}
+
+//action (action creator)
